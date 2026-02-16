83d16621d00bc280b0629735c25e2df770de9453
fukiame <fukiame@proton.me>
2026-02-16 12:06:14 +0700

    /: fry dead WALL_API
    Signed-off-by: fukiame <fukiame@proton.me>

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -102,7 +102,6 @@ class KigyoINIT:
         self.NO_LOAD: List[str] = list(map(str, self.NO_LOAD))
         self.CASH_API_KEY: str = self.parser.get('CASH_API_KEY', None)
         self.TIME_API_KEY: str = self.parser.get('TIME_API_KEY', None)
-        self.WALL_API: str = self.parser.get('WALL_API', None)
         self.LASTFM_API_KEY: str = self.parser.get('LASTFM_API_KEY', None)
         self.WEATHER_API: str = self.parser.get('WEATHER_API', None)
         self.CF_API_KEY: str =  self.parser.get("CF_API_KEY", None)
@@ -152,7 +151,6 @@ WHITELIST_USERS = get_user_list("whitelists")
 SPAMMERS = get_user_list("spammers")
 CASH_API_KEY = KInit.CASH_API_KEY
 TIME_API_KEY = KInit.TIME_API_KEY
-# WALL_API = KInit.WALL_API
 LASTFM_API_KEY = KInit.LASTFM_API_KEY
 WEATHER_API = KInit.WEATHER_API
 CF_API_KEY = KInit.CF_API_KEY
