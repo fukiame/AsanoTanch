693bb32a568b976f3bc504433f1c4ae50dfe1696
fukiame <fukiame@proton.me>
2026-02-16 08:33:52 +0700

    modules: stickers: drop formality
    no other modules have that, make it consistent
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- tg_bot/modules/stickers.py
+++ tg_bot/modules/stickers.py
@@ -29,18 +29,14 @@ def stickerid(update: Update, context: CallbackContext):
     msg = update.effective_message
     if msg.reply_to_message and msg.reply_to_message.sticker:
         update.effective_message.reply_text(
-            "Hello "
-            + f"{mention_html(msg.from_user.id, msg.from_user.first_name)}"
-            + ", The sticker id you are replying is :\n <code>"
+            "The sticker id you are replying is :\n <code>"
             + escape(msg.reply_to_message.sticker.file_id)
             + "</code>",
             parse_mode=ParseMode.HTML,
         )
     else:
         update.effective_message.reply_text(
-            "Hello "
-            + f"{mention_html(msg.from_user.id, msg.from_user.first_name)}"
-            + ", Please reply to sticker message to get id sticker",
+            "Please reply to sticker message to get id sticker",
             parse_mode=ParseMode.HTML,
         )
 
