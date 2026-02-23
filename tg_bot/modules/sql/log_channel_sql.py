2143d6854fc7a4e94a57ab62453e7538e8d730b3
fukiame <fukiame@proton.me>
2026-02-24 00:28:14 +0700

    modules: sql: log_channel: check None
    AttributeError: 'NoneType' object has no attribute 'scalar'
    Signed-off-by: fukiame <fukiame@proton.me>

--- tg_bot/modules/sql/log_channel_sql.py
+++ tg_bot/modules/sql/log_channel_sql.py
@@ -79,7 +79,7 @@ def set_chat_setting(setting: LogChannelSettings):
     try:
         with LOGS_INSERTION_LOCK:
             res: LogChannelSettings = SESSION.query(LogChannelSettings).get(setting.chat_id)
-            if res.scalar():
+            if res is not None and res.scalar():
                 res.log_warn = setting.log_warn
                 res.log_action = setting.log_action
                 res.log_report = setting.log_report
