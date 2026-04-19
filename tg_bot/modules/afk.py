b501e9d10f68d60ac6e5a34e91552ebfe8574e9e
fukiame <fukiame@proton.me>
2026-04-19 10:00:45 +0700

    Revert "modules: afk: add afk time"
    This reverts commit dba3147eddaf01693d50ccf53f8cc79f0cf8bb55.

--- tg_bot/modules/afk.py
+++ tg_bot/modules/afk.py
@@ -1,6 +1,5 @@
 import html
 import random
-import time
 
 from telegram import Update, MessageEntity
 from telegram.ext import Filters, CallbackContext
@@ -19,8 +18,6 @@ def afk(update: Update, _: CallbackContext):
     if not user or user.id in (777000, 1087968824, 136817688):  # ignore channels
         return
 
-    afk_time = int(time.time())
-
     notice = ""
     if len(args) >= 2:
         reason = args[1]
@@ -30,7 +27,7 @@ def afk(update: Update, _: CallbackContext):
     else:
         reason = ""
 
-    sql.set_afk(update.effective_user.id, afk_time, reason)
+    sql.set_afk(update.effective_user.id, reason)
     fname = update.effective_user.first_name
     try:
         update.effective_message.reply_text("{} is now away!{}".format(fname, notice))
@@ -133,43 +130,15 @@ def check_afk(update, user_id, fst_name, userc_id):
         return
     is_afk, reason = sql.check_afk_status(user_id)
     if is_afk:
-        afk_time = sql.get_afk_time(user_id)
-        afk_since = get_readable_time((time.time() - afk_time))
         if not reason:
-            res = "{} is afk since {}".format(fst_name, afk_since)
+            res = "{} is afk".format(fst_name)
             update.effective_message.reply_text(res, parse_mode=None)
         else:
-            res = "{} is afk since {}\nReason: <code>{}</code>".format(
-                html.escape(fst_name), afk_since, html.escape(reason)
+            res = "{} is afk.\nReason: <code>{}</code>".format(
+                html.escape(fst_name), html.escape(reason)
             )
             update.effective_message.reply_text(res, parse_mode="html")
 
-def get_readable_time(seconds: int) -> str:
-    count = 0
-    ping_time = ""
-    time_list = []
-    time_suffix_list = ["s", "m", "h", "days"]
-
-    while count < 4:
-        count += 1
-        if count < 3:
-            remainder, result = divmod(seconds, 60)
-        else:
-            remainder, result = divmod(seconds, 24)
-        if seconds == 0 and remainder == 0:
-            break
-        time_list.append(int(result))
-        seconds = int(remainder)
-
-    for x in range(len(time_list)):
-        time_list[x] = str(time_list[x]) + time_suffix_list[x]
-    if len(time_list) == 4:
-        ping_time += time_list.pop() + ", "
-
-    time_list.reverse()
-    ping_time += ":".join(time_list)
-
-    return ping_time
 
 def __gdpr__(user_id):
     sql.rm_afk(user_id)
